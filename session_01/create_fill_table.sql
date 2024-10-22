7bbb2fb4db0914dbdb003b87a23d4d99a75accc5
manfre-lorson <manfre-lorson@posteo.de>
2024-10-22 18:18:43 +0200

    Create create_fill_table.sql

---
+++ session_01/create_fill_table.sql
@@ -0,0 +1,8 @@
+create table people as (lastname varchar(25), firstname varchar(25), middlenames varchar(50), country varchar(20), country_ios3);
+
+insert into people (lastname, firstname, country, country_iso3) values 
+  ('Wolf', 'Florian', 'Deutschland', 'Deu'), 
+  ('Hofmann', 'Max', 'Germany', 'deu');
+
+
+
